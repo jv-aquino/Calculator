c98a219d7d91204ffcda92e6d17221bf1d122c17
jv-aquino <jvaquinocontact@gmail.com>
2022-05-26 21:19:50 -0300

    Adds non-func UI

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function add (a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  if (operator == "+") {
+    add(a, b);
+  }
+
+  else if (operator == "-") {
+    subtract(a, b);
+  }
+
+  else if (operator == "*" || operator == "x") {
+    multiply(a, b);
+  }
+
+  else if (operator == "/" || operator == "÷") {
+    divide(a, b);
+  }
+}
